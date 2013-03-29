456d84540812edde0e01b2e88a52b45c899323ab
Kyle <kyle@chilcutt.com>
2013-03-29 02:26:37 -0700

    Add more info from my notes
    Add MacOS configurations, and installer for non-clang non-llvm gcc which
    is required for ruby versions < 2.0.0.

--- installers/darwin_installer.sh
+++ installers/darwin_installer.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+darwin_configure_system() {
+  SYSTEM_NAME='athena'
+
+  sudo scutil --set HostName $SYSTEM_NAME
+  sudo scutil --set LocalHostName $SYSTEM_NAME
+  sudo scutil --set ComputerName $SYSTEM_NAME
+}
+
 darwin_update_system() {
   sudo softwareupdate -ia
 }
@@ -15,12 +23,17 @@ darwin_install_xcode() {
 }
 
 darwin_install_macports() {
-  echo 'port'
+  curl https://distfiles.macports.org/MacPorts/MacPorts-2.1.2-10.8-MountainLion.pkg -o $HOME/Downloads/MacPorts.pkg
+  sudo installer -pkg $HOME/Downloads/MacPorts.pkg -target /
 }
 
 darwin_update_macports() {
   sudo port selfupdate
   sudo port upgrade outdated
+}
+
+darwin_install_gcc() {
+  sudo port install apple-gcc42
 }
 
 darwin_install_bash() {
@@ -38,6 +51,34 @@ darwin_install_bash() {
 
 darwin_install_git() {
   sudo port install git-core +bash_completion
+}
+
+darwin_configure_git() {
+  USER='Kyle Chilcutt'
+  EMAIL='kyle@chilcutt.com'
+
+  # set default global settings
+  git config --global user.name $USER
+  git config --global user.email $EMAIL
+  git config --global color.ui true
+  git config --global push.default tracking
+  git config --global core.whitespace space-before-tab,trailing-space
+  git config --global core.editor `which vim`
+  git config --global grep.lineNumber true
+  git config --global core.pager 'less -r'
+
+  # aliases
+  git config --global alias.cp 'cherry-pick'
+  git config --global alias.st 'status -s'
+  git config --global alias.co 'checkout'
+  git config --global alias.br 'branch'
+  git config --global alias.cdiff 'diff --word-diff=color'
+  git config --global alias.graph 'log --oneline --graph --decorate'
+  git config --global alias.ls "log --graph --date=short --format='%C(yellow)%h %C(cyan)%ad %C(reset)%s %C(blue)[%aN]%C(reset)'"
+  git config --global alias.ll "log --graph --date=short --format='%C(yellow)%h %C(cyan)%ad %C(reset)%s %C(blue)[%aN]%C(reset)' --numstat"
+  git config --global alias.la "log --graph --all --date=short --format='%C(yellow)%h %C(cyan)%ad%C(green bold)%d %C(reset)%s %C(blue)[%aN]%C(reset)'"
+  git config --global alias.find '!git ls-files | grep -i'
+  git config --global alias.fgrep 'grep --heading --break --ignore-case'
 }
 
 darwin_install_janus_vim() {
@@ -62,12 +103,15 @@ darwin_install_heroku_toolbelt() {
 }
 
 darwin_installer() {
+  darwin_configure_system
   darwin_update_system
   darwin_install_xcode
   darwin_install_macports
   darwin_update_macports
+  darwin_install_gcc
   darwin_install_bash
   darwin_install_git
+  darwin_configure_git
   darwin_install_janus_vim
   darwin_install_dotfiles
   darwin_install_rvm_ruby
