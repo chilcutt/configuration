7b0de0d9639bbc1e19b72d2e73ec03e0fc3aeb09
Kyle <kyle@chilcutt.com>
2013-03-29 02:28:58 -0700

    Remove individual configurations
    Everything will be handled by the installer script.

--- configs/heroku.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -L https://toolbelt.heroku.com/download/osx -o heroku-toolbelt.pkg
--- configs/vim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# install janus
-curl -Lo- https://bit.ly/janus-bootstrap | bash
-
-# install vim tools
-sudo port install p5-app-ack
-sudo ln -s /opt/local/bin/ack-5.12 /opt/local/bin/ack
-sudo port install ctags
