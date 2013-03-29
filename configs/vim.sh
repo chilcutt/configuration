0782ccb59767f2c8b870e9bb08d53eccaf44afb5
Kyle Chilcutt <kyle@chilcutt.com>
2013-03-29 00:09:46 -0700

    Link ack in /usr/local/bin

--- configs/vim.sh
+++ configs/vim.sh
@@ -5,5 +5,5 @@ curl -Lo- https://bit.ly/janus-bootstrap | bash
 
 # install vim tools
 sudo port install p5-app-ack
-sudo ln -s /opt/local/bin/ack-5.12 /opt/bin/ack
+sudo ln -s /opt/local/bin/ack-5.12 /opt/local/bin/ack
 sudo port install ctags
